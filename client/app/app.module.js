209b940081e80c9a9681c855cf0cc55ff295b95e
Felipe Rêgo <feliperrego@gmail.com>
2016-03-15 16:31:20 -0300

    Configure routes + Include about module

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -2,11 +2,15 @@
 
 // Angular imports
 var angular = require('angular');
+var routesConfig = require('./app.routes');
 require('angular-route');
 require('angular-ui-bootstrap');
 
 // Shared modules imports
 require('./shared/menu/menu.widget');
+
+// Project modules
+require('./components/about/about.controller');
 
 var app = angular.module('app', [
     // Angular Modules
@@ -14,5 +18,24 @@ var app = angular.module('app', [
     'ui.bootstrap',
 
     // Widgets modules
-    'app.widget.menu'
+    'app.widget.menu',
+    'app.about'
 ]);
+
+// Routes configuration
+app.config(['$locationProvider', '$routeProvider', '$controllerProvider', function ($locationProvider, $routeProvider, $controllerProvider) {
+    app.controller = $controllerProvider.register;
+
+    //$routeProvider.otherwise({redirectTo: routesConfig.defaultRoutePath});
+    $locationProvider.html5Mode(true);
+
+    angular.forEach(routesConfig.routes, function (route) {
+        $routeProvider.when(route.url,
+            {
+                url: route.url,
+                templateUrl: route.templateUrl,
+                title: route.title
+            }
+        );
+    });
+}]);
