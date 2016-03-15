8b156172b2d6278c5a62e00975cf8f311aed5a5f
Felipe Rêgo <feliperrego@gmail.com>
2016-03-15 16:44:33 -0300

    Set the page title dynamically.

--- client/app/shared/menu/menu.widget.js
+++ client/app/shared/menu/menu.widget.js
@@ -1,13 +1,23 @@
-angular
-    .module('app.widget.menu', [])
-    .directive("menuWidget", [menuWidget]);
+(function () {
 
-function menuWidget() {
-    return {
-        templateUrl: "app/shared/menu/menu.view.html",
-        restrict: "E",
-        controller: function(){
-            console.log();
+    angular
+        .module('app.widget.menu', [])
+        .directive("menuWidget", [menuWidget]);
+
+    menuWidget.$inject = ['$rootScope'];
+    function menuWidget($rootScope) {
+        // Usage: <menu-widget></menu-widget>
+        return {
+            templateUrl: "app/shared/menu/menu.view.html",
+            restrict: "E",
+            controller: function ($rootScope) {
+                $rootScope.$on('$routeChangeSuccess',
+                    function (event, current, previous) {
+                        $rootScope.pageTitle = current.$$route.title;
+                    }
+                )
+            }
         }
     }
-}
+
+})();
